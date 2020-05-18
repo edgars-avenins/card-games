fca59a583c20d4343acc722efc4798200b183565
Edgars <edgars.avenins@gmail.com>
2020-05-18 18:45:53 +1200

    display the latest dropped card

--- client/components/CardsOld.jsx
+++ client/components/CardsOld.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 
 export const CardsOld = ({deck, setTake}) => {
 
+    function returnLast(){
+      let last = deck.pop()
+      
+      return (
+        <span><img onClick={() => setTake(last)} src={`/images/cards/card-${last.suit}-${last.value}.png`} alt=""/></span>
+      )
+    }
+
+
     return(
         <div>
             {
-              deck &&
-              <span><img onClick={() => setTake(deck)} src={`/images/cards/card-${deck.suit}-${deck.value}.png`} alt=""/></span>
+              deck.length > 0 &&
+              returnLast()
             }
           </div>
     )
