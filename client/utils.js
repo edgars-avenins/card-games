5866c9d3aab3dcc081e30c686ad6f857401e5c1d
Edgars <edgars.avenins@gmail.com>
2020-05-19 18:38:49 +1200

    add img alts and types for dnd

---
+++ client/utils.js
@@ -0,0 +1,29 @@
+export const ItemTypes = {
+    CARD: 'card'
+}
+
+export const altCards = {
+    suit: {
+        'D': 'Diamonds',
+        'H': 'Hearths',
+        'S': 'Spades',
+        'C': 'Clubs'
+    },
+    value: {
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+        10: 'Ten',
+        'J': 'Jack',
+        'Q': 'Queen',
+        'K': 'King',
+        'A': 'Ace',
+        'Joker': 'Joker'
+    }
+}
