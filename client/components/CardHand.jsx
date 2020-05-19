17903ceaf38e057ed1bd912046155b252baaccf3
Edgars <edgars.avenins@gmail.com>
2020-05-19 19:50:52 +1200

    Card moving logic

--- client/components/CardHand.jsx
+++ client/components/CardHand.jsx
@@ -1,9 +1,28 @@
 //cards displayed for single user
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Card } from './Card'
+import update from 'immutability-helper'
 
-export const CardHand = ({cards, setDrop}) => {
+
+export const CardHand = ({cards, setDrop, setCards}) => {
     
+    const moveCard = useCallback(
+        (dragIndex, hoverIndex) => {
+          const dragCard = cards[dragIndex]
+          console.log(dragIndex, hoverIndex)
+          setCards(
+            update(cards, {
+              $splice: [
+                [dragIndex, 1],
+                [hoverIndex, 0, dragCard],
+              ],
+            }),
+          )
+        },
+        [cards],
+        console.log(cards)
+      )
+
     return (
         <>
             <h3>Manas kartis</h3>
@@ -13,7 +32,7 @@ export const CardHand = ({cards, setDrop}) => {
 
                     {
                         cards.map((card, i) => {
-                            return <Card key={i} card={card} setDrop={setDrop}/>
+                            return <Card key={i} moveCard={moveCard} card={card} index={i} setDrop={setDrop}/>
                         })
                     }
                 </div>
