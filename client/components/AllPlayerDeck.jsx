19a6d386a1b58ae8bd42d8a7308f3cc274df18f2
Edgars <edgars.avenins@gmail.com>
2020-05-20 13:03:00 +1200

    deck where all the player combinations meet

---
+++ client/components/AllPlayerDeck.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { altCards } from '../utils'
+
+export const AllPlayerDeck = ({ cards, setAllDeck, socket }) => {
+    const [deck, setDeck] = useState([])
+    if (cards.length != 0) {
+        socket.emit('all deck', cards)
+        cards = []
+        setAllDeck(cards)
+    }
+
+    socket.once('all deck', data => {
+        console.log(data)
+        setDeck([...deck, data])
+    })
+
+    return (
+        <>
+            <h3>All player combinations</h3>
+            {
+                deck.map(combination => (
+                    combination.map((card, i) => {
+                        return <span key={i}>
+                            <img src={`/images/cards/card-${card.suit}-${card.value}.png`} alt={altCards.value[card.value] + ' of ' + altCards.suit[card.suit]} />
+                        </span>
+                    })
+                ))
+            }
+        </>
+    )
+}
