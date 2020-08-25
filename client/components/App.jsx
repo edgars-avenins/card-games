bfe632d52a77ec401f38ad02b34031ad64d635a2
Edgars Avenins <edgarsavenins@pop-os.localdomain>
2020-08-25 14:58:08 +1200

    add conditional render for carddeck

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import io from 'socket.io-client'
-import { CardDeck } from './CardDeck'
-import { ChatBox } from './ChatBox'
+import React, { useState } from "react";
+import io from "socket.io-client";
+import { CardDeck } from "./CardDeck";
+import { ChatBox } from "./ChatBox";
 
 //when deploying io('/')
-let socket = io(':3000')
+let socket = io(":3000");
 
 const App = () => {
-
+  const [start, setStart] = useState(false);
 
   return (
     <>
       <h1>Edgars Card Game</h1>
-      <div className='Dflex'>
+      <div className="Dflex">
         <ChatBox socket={socket} />
-        <button id='start' onClick={() => socket.emit('get cards')}>START</button>
-        <CardDeck socket={socket} />
+        <button
+          id="start"
+          onClick={() => {
+            setStart(true);
+            socket.emit("get cards");
+          }}
+        >
+          START
+        </button>
+        { start && <CardDeck socket={socket} />}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
